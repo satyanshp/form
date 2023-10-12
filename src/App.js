1199e100995cfaf242ec85cfaecf061616536b5d
Yash Diwaker <190104076@hbtu.ac.in>
2023-10-13 00:20:29 +0530

    payment add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {useState} from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
+import axios from 'axios';
 
 function App() {
   const [nameRef,setNameRef] = useState('');
@@ -15,10 +16,81 @@ function App() {
     Address:''
   })
 
-  const onSubmit=()=>{
+  const paymentVerification =
+  async (razorpay_payment_id, razorpay_order_id, razorpay_signature, userDetails) =>{
+    
+     try {
+      const { data } = await axios.post(
+        `http://localhost:8000/api/v1/paymentverification`,
+        {
+          razorpay_payment_id,
+          razorpay_order_id,
+          razorpay_signature,
+          userDetails,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+       );
+       console.log(data);
+     } catch (error) {
+      console.log(error);
+     }
+   };
+  const onSubmit = async () => {
+    
     console.log(formData)
+    const {
+      data: { order},
+    } = await axios.post(
+      `http://localhost:8000/api/v1/create`,
+      {
+   
+        totalAmount: 100,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      }
+    );
+
+    const options = {
+      key: "rzp_test_ctqZo5qbJkPhmO", 
+      amount: order.amount,
+      currency: "INR",
+      name: "Check Wala",
+      description: "Event App",
+      order_id: order.id,
+      handler: function (response) {
+        const { razorpay_payment_id, razorpay_order_id, razorpay_signature } =
+          response;
+
+        
+          paymentVerification(
+            razorpay_payment_id,
+            razorpay_order_id,
+            razorpay_signature,
+            formData
+          )
+        
+      },
+
+      theme: {
+        color: "#9c003c",
+      },
+    };
+    const razorpay = new window.Razorpay(options);
+    razorpay.open();
   }
-  // console.log(formData)
+
+  
+// console.log(formData)
+
   return (
     <div className="App">
       <Box
@@ -35,10 +107,12 @@ function App() {
         <TextField id="outlined-basic" onChange={(e)=>{setFormData({...formData,Name:e.target.value})}} label="Name" variant="outlined"  />
         <TextField id="outlined-basic" onChange={(e)=>{setFormData({...formData,Contact:e.target.value})}} label="Contact" variant="outlined" />
         <TextField id="outlined-basic" onChange={(e)=>{setFormData({...formData,Address:e.target.value})}} label="Address" variant="outlined" />
-        <Button varient='contained' onClick={onSubmit()}>Submit</Button>
+        <Button varient='contained' onClick={() => {
+          onSubmit()
+        }}>Submit</Button>
       </Box>
     </div>
   );
-}
+    }
 
 export default App;
