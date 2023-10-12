ac589c40b793350f33c54971fdc545cdf2b78d5a
satyanshp <satyanshprajapati58@gmail.com>
2023-10-12 23:56:18 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-import {useRef} from 'react'
+import {useState} from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 
 function App() {
-  const nameRef = useRef();
-  const contactRef = useRef();
-  const addressRef = useRef();
+  const [nameRef,setNameRef] = useState('');
+  const [contactRef,setContactRef] = useState('');
+  const [addressRef,setAddressRef] = useState('');
 
+  const [formData,setFormData] = useState({
+    Name:'',
+    Contact:'',
+    Address:''
+  })
+
+  const onSubmit=()=>{
+    console.log(formData)
+  }
+  // console.log(formData)
   return (
     <div className="App">
       <Box
@@ -22,10 +32,10 @@ function App() {
       width={{xs:'100%',sm:'80%',md:'60%'}}
         autoComplete="off"
       >
-        <TextField id="outlined-basic" ref={nameRef} label="Name" variant="outlined" />
-        <TextField id="outlined-basic" ref={contactRef} label="Contact" variant="outlined" />
-        <TextField id="outlined-basic" ref={addressRef} label="Address" variant="outlined" />
-        <Button varient='contained'></Button>
+        <TextField id="outlined-basic" onChange={(e)=>{setFormData({...formData,Name:e.target.value})}} label="Name" variant="outlined"  />
+        <TextField id="outlined-basic" onChange={(e)=>{setFormData({...formData,Contact:e.target.value})}} label="Contact" variant="outlined" />
+        <TextField id="outlined-basic" onChange={(e)=>{setFormData({...formData,Address:e.target.value})}} label="Address" variant="outlined" />
+        <Button varient='contained' onClick={onSubmit()}>Submit</Button>
       </Box>
     </div>
   );
